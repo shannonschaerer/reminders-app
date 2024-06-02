f3add7a31527952aff24de20e046ecab6e56cb43
Shannon Schärer <shannonschaerer@gmail.com>
2024-06-02 20:28:35 +0200

    Added functionality to get data and delete it on the frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 import ReminderList from "./components/ReminderList";
 import Reminder from "./models/Reminder";
+import ReminderService from "./services/Reminder";
 
-const reminders: Reminder[] = [
-
-];
 
 function App() {
-  const [reminders, setReminders] = useState<Reminder[]>([
-    { id: 1, title: "Create todo list" },
-  ]);
+  const [reminders, setReminders] = useState<Reminder[]>([]);
+
+  useEffect(() => {
+    loadReminders();
+  }, []);
+
+  const loadReminders = async () => {
+    const reminders = await ReminderService.getReminders()
+    setReminders(reminders);
+  }
+
+  const removeReminder = async (id: number) => {
+    setReminders(reminders.filter((reminder) => reminder.id !== id))
+  }
   return (
 
     <div className="App">
-      <ReminderList items={reminders} />
+      <ReminderList items={reminders} onRemoveReminder={removeReminder}/>
     </div>
 
   );
--- src/components/ReminderList.tsx
+++ src/components/ReminderList.tsx
@@ -1,19 +1,35 @@
 import React from 'react';
 import Reminder from "../models/Reminder";
-import {ListItem, UnorderedList} from "@chakra-ui/react";
+import {Button, Center, Container, Flex, HStack, ListItem, UnorderedList} from "@chakra-ui/react";
 
 
 interface ReminderListProps {
     items: Reminder[];
+    onRemoveReminder: (id: number) => void;
 }
 
-function ReminderList({ items }: ReminderListProps) {
+function ReminderList({ items, onRemoveReminder }: ReminderListProps) {
     return (
-        <UnorderedList>
-            {items.map((reminder) => (
-                <ListItem key={reminder.id}>{reminder.title}</ListItem>
-            ))}
-        </UnorderedList>
+        <Container>
+            <Center>
+                <UnorderedList>
+                    {items.map((reminder) => (
+                        <ListItem key={reminder.id} py={2}>
+                            <Flex justifyContent="space-between">
+                                {reminder.title}
+                                <Button
+                                    onClick={() => onRemoveReminder(reminder.id)}
+                                    colorScheme="red"
+                                    size="sm"
+                                    variant="outline">
+                                    Remove
+                                </Button>
+                            </Flex>
+                        </ListItem>
+                    ))}
+                </UnorderedList>
+            </Center>
+        </Container>
     );
 }
 
